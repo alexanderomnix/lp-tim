877c8cf6d6598eebf6d681bff699d455b97ff961
alexanderomnix <62620120+alexanderomnix@users.noreply.github.com>
2020-04-14 12:48:21 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import ReactGA from "react-ga";
 ReactGA.initialize("UA-162630651-1");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById("root"));
